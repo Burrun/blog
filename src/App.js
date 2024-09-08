b073dfbd61ce0d97943fbe2669581b7a194c54c0
오현 <127071470+jack3034936@users.noreply.github.com>
2024-09-08 22:31:41 +0900

    1.8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [post, setPost] = useState(["강남", "파이썬", "자바"]);
+  let [rate, setRate] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>ReactBlog</div>
+      </div>
+      {post.map(function (a, i) {
+        //a는 값 , i는 인덱스
+        return (
+          <div className="list" key={i}>
+            <h4>
+              {post[i]}
+              <span
+                onClick={() => {
+                  let copy = [...rate];
+                  copy[i] = copy[i] + 1;
+                  setRate(copy);
+                }}
+              >
+                👍
+              </span>
+              {rate[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+      {
+        (modal = true ? (
+          <Modal color={"yellow"} post={post} setPost={setPost} />
+        ) : null)
+      }
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal" style={{ background: props.color, padding: "20px" }}>
+      <h4>{props.post}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button
+        onClick={() => {
+          let copy = [...props.post];
+          copy[0] = "여자";
+          props.setPost(copy);
+        }}
+      >
+        글수정
+      </button>
     </div>
   );
 }
